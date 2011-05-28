47720d10a14d56b40c046fdbb9501919bbc1dc4b
unknown <aluno@.cesar.intranet>
2011-05-28 15:07:00 -0300

    Tarefa 22: Implements game design to menu screen (in development)

--- android/CatchThePigeonAndroid/gen/br/eng/mosaic/pigeon/client/R.java
+++ android/CatchThePigeonAndroid/gen/br/eng/mosaic/pigeon/client/R.java
@@ -11,22 +11,33 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int facebook=0x7f020000;
-        public static final int facebook_icon=0x7f020001;
-        public static final int icon=0x7f020002;
-        public static final int logo=0x7f020003;
+        public static final int background=0x7f020000;
+        public static final int bnt_start=0x7f020001;
+        public static final int facebook=0x7f020002;
+        public static final int facebook_icon=0x7f020003;
+        public static final int icon=0x7f020004;
+        public static final int logo=0x7f020005;
+        public static final int opening_screen_background=0x7f020006;
+        public static final int picture_of_avatar=0x7f020007;
     }
     public static final class id {
-        public static final int ImageView01=0x7f050000;
-        public static final int date=0x7f050006;
-        public static final int high_score=0x7f050003;
-        public static final int imageView1=0x7f050008;
-        public static final int points=0x7f050007;
-        public static final int progressBar1=0x7f050009;
-        public static final int social_confg=0x7f050004;
-        public static final int start_game=0x7f050001;
-        public static final int top_five=0x7f050002;
-        public static final int userId=0x7f050005;
+        public static final int TableLayout01=0x7f050006;
+        public static final int TableLayout02=0x7f050000;
+        public static final int TableLayout03=0x7f050007;
+        public static final int bt_pos1=0x7f050001;
+        public static final int bt_pos2=0x7f050002;
+        public static final int bt_pos3=0x7f050003;
+        public static final int bt_pos4=0x7f050004;
+        public static final int bt_pos5=0x7f050005;
+        public static final int date=0x7f05000d;
+        public static final int high_score=0x7f05000a;
+        public static final int imageView1=0x7f05000f;
+        public static final int points=0x7f05000e;
+        public static final int progressBar1=0x7f050010;
+        public static final int social_confg=0x7f050008;
+        public static final int start_game=0x7f05000b;
+        public static final int top_five=0x7f050009;
+        public static final int userId=0x7f05000c;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -34,9 +45,14 @@ public final class R {
         public static final int splash=0x7f030002;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int high_score=0x7f040003;
-        public static final int start_game=0x7f040001;
-        public static final int top_five=0x7f040002;
+        public static final int app_name=0x7f040005;
+        public static final int high_score=0x7f040008;
+        public static final int pos1=0x7f040000;
+        public static final int pos2=0x7f040001;
+        public static final int pos3=0x7f040002;
+        public static final int pos4=0x7f040003;
+        public static final int pos5=0x7f040004;
+        public static final int start_game=0x7f040006;
+        public static final int top_five=0x7f040007;
     }
 }
---
+++ android/CatchThePigeonAndroid/src/br/eng/mosaic/pigeon/client/gameplay/cast/Figeon.java
@@ -0,0 +1,12 @@
+package br.eng.mosaic.pigeon.client.gameplay.cast;
+
+import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
+
+public class Figeon extends Pigeon {
+
+	public Figeon(float pX, float pY, TiledTextureRegion pTextureRegion, int life) {
+		super(pX, pY, pTextureRegion, life);
+		// TODO Auto-generated constructor stub
+	}
+
+}
