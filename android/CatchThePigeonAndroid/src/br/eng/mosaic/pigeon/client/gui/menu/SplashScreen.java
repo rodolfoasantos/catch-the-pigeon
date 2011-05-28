ecbaca8c60c035abcc325c82a194c72d7b8de795
leonardoaraujo85 <leonardoaraujo85@gmail.com>
2011-05-28 16:58:26 -0300

    Improved SplashScreen. We created two xml files that deal with fade_in and fade_out. Also added a picture with the logo of MOSAIC, for SplashScreen. The figure of the old UFPe / CIN was not deleted.

--- android/CatchThePigeonAndroid/src/br/eng/mosaic/pigeon/client/gui/menu/SplashScreen.java
+++ android/CatchThePigeonAndroid/src/br/eng/mosaic/pigeon/client/gui/menu/SplashScreen.java
@@ -1,28 +1,49 @@
 package br.eng.mosaic.pigeon.client.gui.menu;
 
-import br.eng.mosaic.pigeon.client.R;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import br.eng.mosaic.pigeon.client.R;
 
 public class SplashScreen extends Activity implements Runnable{
+		   //create name of animation
+		Animation myFadeInAnimation;
+		Animation myFadeOutAnimation;
+		ImageView myImageView;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.splash);
-		Handler h = new Handler();
-		h.postDelayed(this, 2000);
-	}
-	
-	@Override
-	public void run() {
-		startActivity(new Intent(this,MainActivity.class));
-		finish();
-		
-	}
-		
+		/** Called when the activity is first created. */
+		@Override
+		public void onCreate(Bundle savedInstanceState) {
+		    super.onCreate(savedInstanceState);
+		    setContentView(R.layout.splash);
 
+		 //grab the imageview and load the animations
+		    
+		     myFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+		     myFadeOutAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+		     myImageView = (ImageView) findViewById(R.id.imageView1);
+		  
+		//fade it in, and fade it out. 
+		    Handler h = new Handler();
+		    myImageView.startAnimation(myFadeInAnimation);
+		   
+			h.postDelayed(this, 4000);
+		    
+		     }
+
+		@Override
+		public void run() {
+			
+			myImageView.startAnimation(myFadeOutAnimation);
+			startActivity(new Intent(this,MainActivity.class));
+			finish();
+			
+		}
 }
+
+		
+
