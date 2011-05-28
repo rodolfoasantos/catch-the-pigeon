d29348568e10e5f46263585178b41e263a0aaa18
rodolfoasantos <rodolfoasantos@gmail.com>
2011-05-28 18:37:12 -0300

    Optimized method of enemys generation, creation position and atack

--- android/CatchThePigeonAndroid/src/br/eng/mosaic/pigeon/client/gameplay/util/GameUtil.java
+++ android/CatchThePigeonAndroid/src/br/eng/mosaic/pigeon/client/gameplay/util/GameUtil.java
@@ -6,34 +6,39 @@ import java.util.Random;
 import org.anddev.andengine.opengl.texture.region.TiledTextureRegion;
 
 import br.eng.mosaic.pigeon.client.gameplay.cast.BadPigeon;
+import br.eng.mosaic.pigeon.client.gameplay.cast.Pigeon;
 
 public class GameUtil {
 	
 	
-	public static ArrayList<BadPigeon> genEnemies(int amount, int posX, int posY, TiledTextureRegion texture){
+	public static ArrayList<BadPigeon> genEnemies(int amount, int cameraWidth, int cameraHeight, TiledTextureRegion texture){
 		
 		ArrayList<BadPigeon> bads = new ArrayList<BadPigeon>();
 		Random r = new Random();
 				
 		for (int i = 0; i < amount; i++) {
 			
+			int posAnim;
+			
 			switch (r.nextInt(4)) {
 			
 			case 0:
-				bads.add(new BadPigeon( ((int)(Math.random() * posX)), posY, texture, 9, 11, 1));
+				posAnim= ((int)(Math.random() * cameraWidth));
+				bads.add(new BadPigeon( posAnim, cameraHeight, texture, (posAnim < Pigeon.posX ? 0 : 9) , (posAnim < Pigeon.posX ? 2 : 11), 1));
 				break;
 				
 			case 1:
-				bads.add(new BadPigeon( posX, ((int)(Math.random() * posY)), texture, 9, 11, 1));
+				bads.add(new BadPigeon( cameraWidth, ((int)(Math.random() * cameraHeight)), texture, 9, 11, 1));
 				break;
 				
 			case 2:
-				bads.add(new BadPigeon( 0, ((int)(Math.random() * posY)), texture, 9, 11, 1));
+				posAnim = ((int)(Math.random() * cameraWidth));
+				bads.add(new BadPigeon(((int)(Math.random() * cameraWidth)), 0, texture, (posAnim < Pigeon.posX ? 0 : 9) , (posAnim < Pigeon.posX ? 2 : 11), 1));
 				break;
 				
 			case 3:
-				BadPigeon b = new BadPigeon(0, ((int)(Math.random() * posY)), texture, 9, 11, 1);
-				b.velocity*=1.2;
+				BadPigeon b = new BadPigeon(0, ((int)(Math.random() * cameraHeight)), texture, 0, 2, 1);
+				//b.velocity*=0.2;
 				bads.add(b);
 				break;
 				
